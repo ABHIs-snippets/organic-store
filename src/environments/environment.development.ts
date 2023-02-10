11ab160e2271ccea96b6a10d86c2f32fe2ab9381
Abhinav1923 <abhinavofficial120@gmail.com>
2023-02-10 22:38:18 +0530

    New branch added

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,3 @@
 export const environment = {
-<<<<<<< HEAD
   baseUrl:'https://organic-store-api.onrender.com/api/v1/'
-=======
-  // baseUrl:'https://organic-store-api.onrender.com/api/v1/'
-  baseUrl:'http://localhost:5000/api/v1/'
->>>>>>> 9647d060209748b79f201c97c9cfa4b5f31c890b
 };
